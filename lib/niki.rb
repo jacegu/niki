da9a04cef7ef233f62db7d5018a0d587fdc82f26
Javier Acero <j4cegu@gmail.com>
2011-08-04 20:52:12 +0200

    niki handles pages

--- lib/niki.rb
+++ lib/niki.rb
@@ -1,3 +1,17 @@
-class Niki
+require 'page'
 
+class Niki
+  attr_reader :pages
+
+  def initialize
+    @pages = []
+  end
+
+  def add_page(page)
+    @pages << page
+  end
+
+  def has_pages?
+    pages.any?
+  end
 end
--- spec/niki_spec.rb
+++ spec/niki_spec.rb
@@ -1,2 +1,33 @@
+require 'spec_helper'
+
 describe Niki do
+  before do
+    @niki = Niki.new
+  end
+
+  describe 'when created' do
+    it 'has no pages' do
+      @niki.pages.must_be_empty
+    end
+  end
+
+  describe '#add_page page' do
+    it 'adds the page to the niki' do
+      page = stub
+      @niki.add_page(page)
+      @niki.pages.must_include page
+    end
+  end
+
+  describe '#has_pages?' do
+    it 'returns false if niki has no pages' do
+      @niki.has_pages?.must_equal false
+    end
+
+    it 'returns true if niki has any pages' do
+      @niki.add_page(stub)
+      @niki.has_pages?.must_equal true
+    end
+
+  end
 end
