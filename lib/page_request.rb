3862ff5b36dc64752c020e30e8fbce127f3a15d7
Javier Acero <j4cegu@gmail.com>
2011-08-17 22:26:02 +0200

    page request knows when all pages are requested

--- lib/page_request.rb
+++ lib/page_request.rb
@@ -1,7 +1,13 @@
 class PageRequest
   attr_reader :path
 
+  ALL_PAGES_PATH = '/pages'
+
   def initialize(path)
     @path = path
+  end
+
+  def all_pages?
+    path == ALL_PAGES_PATH or path == "#{ALL_PAGES_PATH}/"
   end
 end
--- spec/page_request_spec.rb
+++ spec/page_request_spec.rb
@@ -3,9 +3,29 @@ require 'page_request'
 describe PageRequest do
   describe 'a new page request' do
     it 'is created witha path' do
-      the_path = '/all'
+      the_path = '/some-path'
       requested = PageRequest.new(the_path)
       requested.path.must_equal the_path
     end
   end
+
+  describe '#all_pages?' do
+    it 'returns true if requested path is "/pages"' do
+      all_pages_path = '/pages'
+      requested = PageRequest.new(all_pages_path)
+      requested.all_pages?.must_equal true
+    end
+
+    it 'returns true if requested path is "/pages/"' do
+      all_pages_path = '/pages/'
+      requested = PageRequest.new(all_pages_path)
+      requested.all_pages?.must_equal true
+    end
+
+    it 'returns false for every other path' do
+      requested = PageRequest.new('/somepath')
+      requested.all_pages?.must_equal false
+    end
+  end
+
 end
