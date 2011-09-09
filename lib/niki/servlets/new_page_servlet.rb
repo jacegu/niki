2f963f3126f91debee5b9a8ed964b324371b70ee
Javier Acero <j4cegu@gmail.com>
2011-09-09 10:32:28 +0200

    little refactoring

--- lib/niki/servlets/new_page_servlet.rb
+++ lib/niki/servlets/new_page_servlet.rb
@@ -7,11 +7,9 @@ module Niki
 
       def do_POST(request, response)
         niki = @options[0]
-        @title = request.query['title']
-        @content = request.query['content']
+        @title, @content  = request.query['title'], request.query['content']
         if Page.would_be_valid_with_title?(@title)
-          page = Page.with(@title, @content)
-          niki.add_page(page)
+          niki.add_page(Page.with(@title, @content))
           redirect_to_page(page, response)
         else
           @error_message = 'every niki must have a title'
