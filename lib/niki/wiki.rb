fbd829459b7b76265d47f89c8ecb257264df63dc
Javier Acero <j4cegu@gmail.com>
2011-12-17 11:53:41 +0100

    improved page finding based on a hash

--- lib/niki/wiki.rb
+++ lib/niki/wiki.rb
@@ -18,23 +18,41 @@ module Niki
     end
 
     def has_a_page_entitled?(title)
-      page_with_title(title).found?
+      find_page_with(title: title).found?
     end
 
     def has_a_page_with_url?(url)
-      page_with_url(url).found?
+      find_page_with(url: url).found?
     end
 
     def page_with_url(url)
-      find_page_by{ |p| p.url == url }
+      find_page_with(url: url)
     end
 
     def page_with_title(title)
-      find_page_by{ |p| p.title == title }
+      find_page_with(title: title)
     end
 
-    def find_page_by(&criteria)
-      pages.select(&criteria).first || NullPage.new
+    def find_page_with(page_info)
+      pages.select(&Criteria.for(page_info)).first || NullPage.new
+    end
+
+  end
+
+  class Criteria
+    attr_reader :field, :value
+
+    def self.for(page_info)
+      new(page_info).build_proc
+    end
+
+    def initialize(page_info)
+      @field = page_info.keys.first
+      @value = page_info[field].gsub("'", "\\'")
+    end
+
+    def build_proc
+      Proc.new { |p| eval "p.#{@field} == '#{@value}'" }
     end
   end
 end
